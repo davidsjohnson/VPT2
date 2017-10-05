25ca07a44861e98a142093f54b49454e1f21a1c9
dsj <fortjay81@gmail.com>
2017-10-04 23:48:50 -0700

    Update Annotation Processing

--- vpt/utils/data_scripts/color_threshold.py
+++ vpt/utils/data_scripts/color_threshold.py
@@ -6,9 +6,10 @@ from vpt.streams.file_stream import *
 
 def run():
 
-    folder = "data/posture/p4/p4c/masks"
+    og_folder = "data/rdf/p4/cae_masks/og"
+    mask_folder = "data/rdf/p4/cae_masks/masks"
     ftype = ".bmp"
-    fs = FileStream(folder, ftype)
+    fs = FileStream(og_folder, ftype)
 
     # values from testing with imageJ
     l_bound = np.array([4, 66, 125])
@@ -20,11 +21,19 @@ def run():
 
         img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(img_hsv, l_bound, u_bound)
-        mask = cv2.GaussianBlur(mask, (3,3), 0)
+        # mask = cv2.GaussianBlur(mask, (3,3), 0)
         mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
 
-        new_name = fname.strip(".bmp") + ".jpg"
-        cv2.imwrite(new_name, mask)
+        temp = fname.split("/")
+        filename = temp[-1].strip(ftype) + ".jpg"
+        exercise = temp[-2]
+        new_path = os.path.join(mask_folder, exercise)
+
+        if not os.path.exists(new_path):
+            os.mkdir(new_path)
+
+        new_path = os.path.join(new_path, filename)
+        cv2.imwrite(new_path, mask)
 
 
 if __name__ == "__main__":
