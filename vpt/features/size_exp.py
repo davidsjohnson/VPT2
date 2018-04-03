09395914485f75f7d185ab46bbacf7323f39749f
dsj <fortjay81@gmail.com>
2018-04-03 15:56:32 -0700

    Size Experiment without Block Norm

--- vpt/features/size_exp.py
+++ vpt/features/size_exp.py
@@ -43,8 +43,8 @@ def data_gen(hs, feature_func, pixels_per_cell=(8,8), cells_per_block=(3,3), img
 
             # TODO: Add smoothing??
 
-            x_lh = feature_func(lh_dmap, pixels_per_cell=pixels_per_cell, cells_per_block=cells_per_block, block_norm="L1-sqrt", img_size=img_size, pad_img=False)
-            x_rh = feature_func(rh_dmap, pixels_per_cell=pixels_per_cell, cells_per_block=cells_per_block, block_norm="L1-sqrt", img_size=img_size, pad_img=False)
+            x_lh = feature_func(lh_dmap, pixels_per_cell=pixels_per_cell, block_norm=None, img_size=img_size, pad_img=False)
+            x_rh = feature_func(rh_dmap, pixels_per_cell=pixels_per_cell, block_norm=None, img_size=img_size, pad_img=False)
 
             X_lh.append(x_lh)
             y_lh.append(lh_label)
@@ -59,13 +59,11 @@ def data_gen(hs, feature_func, pixels_per_cell=(8,8), cells_per_block=(3,3), img
 
 def main():
 
-    features = [hog]
-    cell_sizes = [(4,4), (6,6), (8,8), (12,12), (16,16)]
-    block_sizes = [(1,1), (2,2), (3,3), (4,4)]
+    features = [honv]
+    cell_sizes = [(8,8), (12,12), (16,16)]
 
     for f in features:
         for c in cell_sizes:
-            for b in block_sizes:
 
                 ## RDF Parameters
                 M = 5
@@ -76,13 +74,13 @@ def main():
                     fname = "honv"
                 else:
                     fname = "hog"
-                feature_type = "f_{}-c_{}-b_{}".format(fname, c[0], b[0])
+                feature_type = "f_{}-c_{}-b_none".format(fname, c[0])
 
                 data_type = "train"
 
                 hs = get_handstream(M, radius, participants)
-                print("Extracting Features:", fname, "- Pixels per Cell:", c, "- Cells per Block:", b)
-                X_lh, y_lh, X_rh, y_rh, filenames = data_gen(hs, f, pixels_per_cell=c, cells_per_block=b)
+                print("Extracting Features:", fname, "- Pixels per Cell:", c, "- No Block Norm",)
+                X_lh, y_lh, X_rh, y_rh, filenames = data_gen(hs, f, pixels_per_cell=c, )
                 print("Saving Data...")
                 save_data(X_lh, y_lh, X_rh, y_rh, filenames, M, radius, feature_type, data_type=data_type,  participants="all_participants")
 
